ca67bf627bc6d67462b23c1fb5ff86fa5d592e63
akashpundir931 <akashpundir931@gmail.com>
2020-10-11 15:03:13 +0530

    js

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 var i = 0;
+var images = [];
+var time = 3000;
+
+images[0] = "img/slide1.png";
+images[1] = "img/slide2.png";
+images[2] = "img/res.jpg";
+
+function changeImg(){
+    document.slide.src = images[i];
+    
+    if(i < images.length - 1){
+        i++;
+    }
+    else{
+        i = 0;
+    }
+    setTimeout("changeImg()", time)
+}
+window.onload = changeImg;
