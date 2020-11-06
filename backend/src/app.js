27f2c0b2ba5b3cbfe98b083b4847f9ed00a7e579
akashpundir931 <akashpundir931@gmail.com>
2020-11-06 22:32:12 +0530

    about

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,4 @@
 const mongoose = require("mongoose");
-
 mongoose.connect("mongodb://localhost:27017/test" ,{ useUnifiedTopology: true , useNewUrlParser: true })
 .then( ()=> console.log("connection successfully")).catch( () => console.log("error"));
 
@@ -7,12 +6,8 @@ mongoose.connect("mongodb://localhost:27017/test" ,{ useUnifiedTopology: true , 
 
 const schemaPlay  = new mongoose.Schema({
 
-    name: String,
-    type: String,
-    roll_no: Number,
+    
 });
 
 //model
 const Test = new mongoose.model("Test",schemaPlay);
-
-
