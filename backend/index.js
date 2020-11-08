02c9e54d793c4cf9aab2dfbca1df63f2286bd4f5
akashpundir931 <akashpundir931@gmail.com>
2020-11-08 20:51:16 +0530

    great

--- backend/index.js
+++
@@ -1,2 +0,0 @@
-var mongoose = require("mongoose");
-mongoose.connect(mongo:localhost/3000/test)
--- script.js
+++ script.js
@@ -7,15 +7,24 @@ images[1] = "img/2.png";
 images[2] = "img/3.png";
 images[3] = "img/4.png";
 
-function changeImg(){
+function changeImg() {
     document.slide.src = images[i];
-    
-    if(i < images.length - 1){
+
+    if (i < images.length - 1) {
         i++;
-    }
-    else{
+    } else {
         i = 0;
     }
     setTimeout("changeImg()", time)
 }
 window.onload = changeImg;
+
+// Side menu START
+function openNav() {
+    document.getElementById("mySidenav").style.width = "300px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
+// Side menu END
